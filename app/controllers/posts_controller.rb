0287d2f80116326a67d97d3be500400ecdffc63a
nowallcloud9_007 <nowallcloud9+007@gmail.com>
2017-01-29 07:27:34 +0000

    コントローラー実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,22 +1,17 @@
 class PostsController < ApplicationController
-  def index
-  end
-
-  def show
-  end
-
   def new
+    @post = Post.new # フォーム用の空のインスタンスを生成する。
   end
 
   def create
+    @post = Post.new(post_params) # ストロングパラメータを引数に
+    @post.save # saveをしてデータベースに保存する。
+    redirect_to "/posts/#{@post.id}" # showページにリダイレクト
   end
 
-  def edit
-  end
+  private
 
-  def update
-  end
-
-  def destroy
+  def post_params # ストロングパラメータを定義する
+    params.require(:post).permit(:title, :body, :category)
   end
 end
