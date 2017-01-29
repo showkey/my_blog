7baefb33ff4b7640e2de3849fd6d9284c3fd1d3a
nowallcloud9_007 <nowallcloud9+007@gmail.com>
2017-01-29 08:04:53 +0000

    Viewの修正

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,12 +1,11 @@
-
-class PostsController < ApplicationController
+  class PostsController < ApplicationController
+  before_action :set_post, only: [ :show, :edit, :update, :destroy]
   def index
-    @posts = Post.all
-    @new_posts = Post.all
+    @posts = Post.order(created_at: :desc)
+    # 最新記事を５つ取得
+    @new_posts = Post.order(created_at: :desc).limit(5)
   end
-
   def show
-    @post = Post.find(params[:id])
   end
 
   def new
@@ -20,18 +19,15 @@ class PostsController < ApplicationController
   end
 
   def edit
-    @post = Post.find(params[:id])
   end
 
   def update
-    @post = Post.find(params[:id])
     @post.update(post_params)
     redirect_to "/posts/#{@post.id}"
 
   end
 
   def destroy
-    @post = Post.find(params[:id])
     @post.destroy
     redirect_to "/posts"
   end
@@ -41,5 +37,8 @@ class PostsController < ApplicationController
   def post_params
     params.require(:post).permit(:title, :body, :category)
   end
-end
 
+  def set_post
+    @post = Post.find(params[:id])
+  end
+end
